baca6ab64db0b5a9ee4057bd96e56249df2841eb
Aidan Tiu <tiuprof77@gmail.com>
2024-05-04 17:19:30 +0800

    Add and validate operations

--- simple_calculator.py
+++ simple_calculator.py
@@ -4,5 +4,15 @@
 # Ask user if they want to continue
 # If yes, repeat the process. If no, exit the program
 
+try:
+    # Loop to execute the program
+    while True:
+        
+        # Ask user to choose an operation
+        get_operation = input("Choose an operation: add, subtract, multiply, divide: ")
 
+        # Check if the operation is valid using try
+        if get_operation not in ['add', 'subtract', 'multiply', 'divide'].upper():
+            print("Choose within the four operation. Please try again.")
+            continue
 
