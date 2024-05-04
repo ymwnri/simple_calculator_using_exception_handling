7147431c6e77de2b8f0d3aa5be68ca28a8759286
Aidan Tiu <tiuprof77@gmail.com>
2024-05-04 17:09:33 +0800

    Add pseudocode

---
+++ simple_calculator.py
@@ -0,0 +1,8 @@
+# Ask user to choose one operation from add, subtract, multiply, divide
+# Ask for user input for two numbers
+# Use exception handling to catch any errors
+# Ask user if they want to continue
+# If yes, repeat the process. If no, exit the program
+
+
+
